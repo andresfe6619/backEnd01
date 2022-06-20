2da6adb66e92cf155cae5fc3f872062acd3d5b78
andresfe6619 <andresfe6619@gmail.com>
2022-06-20 15:08:35 -0500

    HandleBars

--- Rutas/index.js
+++ Rutas/index.js
@@ -1,61 +1,17 @@
 
-const express= require('express');
-const app = express();
-const puerto = 8080
+const express = require('express');
+
 const moment = require('moment');
 const fs = require('fs'); 
-const res = require('express/lib/response');
+
 const {Router} = require('express');
 const router = Router();
 const Productos = require("./Productos.js");
 
 
 
-// deleteById(Id) {
-    
-    
-//     const deleteId = async () => {  
-//        try{
-//         const data = JSON.parse(await  fs.promises.readFile("./contenedor.txt", "utf-8" ))
-       
-     
-//        const product = data.find(product => product.id === Id);
-//        const newData = data.filter(product => product.id !== Id);
-//        fs.promises.writeFile("./contenedor.txt",JSON.stringify( newData ) )
-//        console.log("Producto eliminado por id")
-//        return ( product ?  console.log( product): console.log("El producto no existe"));
-        
-//    } catch(error) {
-//                  console.log(error);
-//              }
-//     }    
-           
-//      setTimeout(deleteId, 6000);
-
-// }
-// deleteAll() {
-    
-    
-//     const deleteAll = async () => {  
-//        try{
-//         const data = JSON.parse(await  fs.promises.readFile("./contenedor.txt", "utf-8" ))
-       
-     
-//        const newData = [];
-//        fs.promises.writeFile("./contenedor.txt",JSON.stringify( newData ) )
-//        console.log("Todos los Productos han sido eliminados")  
-//    } catch(error) {
-//                  console.log(error);
-//              }
-//     }    
-           
-//      setTimeout(deleteAll, 7000);
-// }}
-
-
-
 router.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html');
+    res.render('form');
     ;
 });
 let visitas=0
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
-
-
-
-
 const app = express();
+
 const rutas = require('./Rutas/index.js');
+const path = require('path')
+
+const { engine } = require('express-handlebars')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use("/api", rutas);
-app.listen(8080, () => {
 
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: path.join(__dirname, './views/layouts/main.hbs'),
+    layoutsDir: path.join(__dirname, './views/layout'),
+    partialsDir: path.join(__dirname, './views/partials')
+}))
+
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'hbs')
+
+
+
+
+app.use("/api", rutas);
+
+app.listen(8080, () => {
     console.log('Servidor corriendo en el puerto 8080');
 })
